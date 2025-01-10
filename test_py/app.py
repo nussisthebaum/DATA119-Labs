e3e3fd9acc3a3181547019dc21ceb880ecd9b608
Amanda Kube Jotte <amandakube95@gmail.com>
2025-01-10 16:50:52 -0600

    adding small py example

---
+++ test_py/app.py
@@ -0,0 +1,17 @@
+import shiny
+
+app_ui = ui.page_fluid(
+    ui.input_slider("n", "Number of bins", min=10, max=100, value=30),
+    ui.output_plot("hist"),
+)
+
+def server(input, output, session):
+    @output
+    @render.plot
+    def hist():
+        import numpy as np
+        import matplotlib.pyplot as plt
+        x = np.random.randn(1000)
+        plt.hist(x, bins=input.n())
+
+app = shiny.App(app_ui, server)
